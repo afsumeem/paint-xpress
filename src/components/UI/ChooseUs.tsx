f6b8a3a750c2670f3165c1e41beff09754e8f5ba
Afsana Meem <afsumeem@gmail.com>
2023-10-15 15:17:37 +0600

    benefits section done

--- src/components/UI/ChooseUs.tsx
+++ src/components/UI/ChooseUs.tsx
@@ -14,101 +14,111 @@ import worker from "@/assests/images/images.png";
 
 const ChooseUs = () => {
   return (
-    <div>
-      <h3>Benefits</h3>
-      <h1>Why Choose us</h1>
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<BarsOutlined />}
-        >
-          <div className="flex justify-between">
-            <div className="">
-              {/* img */}
+    <div className="bg-slate-100">
+      <div className="px-10 pt-10">
+        <div className="mb-6">
+          <h4 className="font-bold text-2xl text-sky-400 uppercase ">
+            Benefits
+          </h4>
+          <h2 className="text-4xl font-mono font-semibold uppercase">
+            Why Choose us
+          </h2>
+        </div>
 
-              <Image
-                src={waterImg}
-                alt="water proofing"
-                height={100}
-                width={100}
-                className="rounded-full"
-              />
-              <h3 className="vertical-timeline-element-title">
-                Water Proofing
-              </h3>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work mb-0"
+            contentStyle={{
+              border: "1px dashed #F7F7F7",
+              color: "#fff",
+            }}
+            contentArrowStyle={{ borderRight: "7px solid  gray" }}
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          >
+            <div className="flex flex-col md:flex-row items-center justify-around">
+              <div className="border m-4 py-4 px-8 transition duration-700  ">
+                {/* img */}
+
+                <Image
+                  src={waterImg}
+                  alt="water proofing"
+                  height={100}
+                  width={100}
+                  // className="rounded-full"
+                />
+                <h3 className="vertical-timeline-element-title text-black">
+                  Water Proofing
+                </h3>
+              </div>
+              <div className="border m-4 py-4 px-8 transition duration-700">
+                <Image
+                  src={fauxFinish}
+                  alt="water proofing"
+                  height={100}
+                  width={100}
+                  // className="rounded-full"
+                />
+                <h3 className="vertical-timeline-element-title text-black">
+                  Faux Finishes
+                </h3>
+              </div>
+              <div className="border m-4 py-4 px-8 transition duration-700">
+                <Image
+                  src={prePaint}
+                  alt="water proofing"
+                  height={100}
+                  width={100}
+                  // className="rounded-full"
+                />
+                <h3 className="vertical-timeline-element-title text-black">
+                  Pre-Paint Demo
+                </h3>
+              </div>
             </div>
-            <div className="">
-              <Image
-                src={fauxFinish}
-                alt="water proofing"
-                height={100}
-                width={100}
-                className="rounded-full"
-              />
-              <h3 className="vertical-timeline-element-title">Faux Finishes</h3>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<BarsOutlined />}
+          >
+            <div className="flex flex-col md:flex-row items-center justify-around">
+              <div className="border m-4 py-4 px-8 transition duration-700  ">
+                <Image
+                  src={qauality}
+                  alt="water proofing"
+                  height={100}
+                  width={100}
+                />
+                <h3 className="vertical-timeline-element-title">
+                  Quality Material
+                </h3>
+              </div>
+              <div className="border m-4 py-4 px-8 transition duration-700  ">
+                <Image
+                  src={worker}
+                  alt="water proofing"
+                  height={100}
+                  width={100}
+                />
+                <h3 className="vertical-timeline-element-title">
+                  Trained Workers
+                </h3>
+              </div>
+              <div className="border m-4 py-4 px-8 transition duration-700  ">
+                <Image
+                  src={response}
+                  alt="water proofing"
+                  height={100}
+                  width={100}
+                />
+                <h3 className="vertical-timeline-element-title">
+                  Quick Response
+                </h3>
+              </div>
             </div>
-            <div className="">
-              <Image
-                src={prePaint}
-                alt="water proofing"
-                height={100}
-                width={100}
-                className="rounded-full"
-              />
-              <h3 className="vertical-timeline-element-title">
-                Pre-Paint Demo
-              </h3>
-            </div>
-          </div>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<BarsOutlined />}
-        >
-          <div className="flex justify-between">
-            <div className="">
-              <Image
-                src={qauality}
-                alt="water proofing"
-                height={100}
-                width={100}
-                className="rounded-full"
-              />
-              <h3 className="vertical-timeline-element-title">
-                Quality Material
-              </h3>
-            </div>
-            <div className="">
-              <Image
-                src={worker}
-                alt="water proofing"
-                height={100}
-                width={100}
-                className="rounded-full"
-              />
-              <h3 className="vertical-timeline-element-title">
-                Trained Workers
-              </h3>
-            </div>
-            <div className="">
-              <Image
-                src={response}
-                alt="water proofing"
-                height={100}
-                width={100}
-                className="rounded-full"
-              />
-              <h3 className="vertical-timeline-element-title">
-                Quick Response
-              </h3>
-            </div>
-          </div>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
     </div>
   );
 };
