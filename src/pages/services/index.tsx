2601c0a9a194c45c4bd8eab28da2a7915a45e671
Afsana Meem <afsumeem@gmail.com>
2023-10-15 18:16:57 +0600

    service page done

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 import RootLayout from "@/components/Layouts/RootLayout";
 import { useGetServicesQuery } from "@/redux/features/services/serviceApi";
 import { IServices } from "@/types/global";
-import Image from "next/image";
+import { Col, Row, Spin, message } from "antd";
+import { PhoneOutlined } from "@ant-design/icons";
 import Link from "next/link";
 import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
+import { addToBookingList } from "@/redux/features/services/serviceSlice";
 
 //
 
@@ -23,30 +27,179 @@ const ServicePage = ({ services, categories }: IProps) => {
     category: selectCategory,
   });
 
-  return (
-    <div>
-      <h5>What We Do</h5>
-      <h1>Latest Projects</h1>
+  const dispatch = useAppDispatch();
 
-      <form className="join mx-auto w-2/3">
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  // const handleAddToBookingList = (service: IServices) => {
+  //   dispatch(addToBookingList(service));
+  //   message.success("Service booked");
+  // };
+
+  const selectedService = useAppSelector((state) => state.bookingList.services);
+
+  const handleAddToBookingList = (service: IServices) => {
+    // if (!buttonClicked) {
+    dispatch(addToBookingList(service));
+    message.success("Service booked");
+    // setButtonClicked(true);
+    // } else {
+    //   message.warning("Service already booked");
+    // }
+  };
+
+  return (
+    <div className="mx-10 mt-10">
+      <div className="mb-10 section-title">
+        <h4 className="font-bold text-md text-sky-400 uppercase ">
+          Our Services
+        </h4>
+        <h2 className="text-2xl font-semibold uppercase font-mono">
+          We Provide Superior Paint Service
+        </h2>
+        <hr />
+      </div>
+
+      <Row gutter={20}>
+        <Col sm={24} md={6} lg={6}>
+          <form className=" my-2">
+            <label htmlFor="" className="font-semibold text-lg">
+              {" "}
+              Search Services
+            </label>
+            <input
+              type="text"
+              onChange={(e) => setSearchText(e.target.value)}
+              className=" w-full py-2 border rounded-md border-black px-2"
+              placeholder="Search Services"
+            />
+          </form>
+          <hr />
+          <h2 className="my-4 font-semibold text-lg">Select a Category</h2>
+          {categories?.map((category, i) => (
+            <div key={i}>
+              <input
+                onChange={() => setSelectCategory(category)}
+                className="h-3 w-3"
+                id={category}
+                type="radio"
+                name="category"
+                checked={selectCategory === category}
+              />
+              <label className="text-[14px] ml-4" htmlFor={category}>
+                {category}
+              </label>
+            </div>
+          ))}
+          <button
+            onClick={() => {
+              setSelectCategory("");
+            }}
+            className=" mt-4 w-full rounded-none text-blue-950 hover:text-white py-2   bg-sky-400 hover:bg-sky-950 transition duration-1000"
+          >
+            Reset category
+          </button>
+        </Col>
+        <Col sm={24} md={18} lg={18}>
+          {data?.length === 0 ? (
+            <h2 className="font-mono text-4xl text-center mt-40 text-red-500">
+              Data not found!!
+            </h2>
+          ) : (
+            <>
+              {isLoading ? (
+                <Spin size="large" />
+              ) : (
+                <Row gutter={20}>
+                  {data?.map((service: IServices, i: number) => (
+                    <Col
+                      key={i}
+                      span={6}
+                      data-aos="zoom-in-up"
+                      data-aos-duration="1200"
+                      xs={24}
+                      sm={24}
+                      md={12}
+                      lg={12}
+                      xl={8}
+                      className="section-card mb-10  gutter-row"
+                    >
+                      <div className="mt-4">
+                        <div className="bg-black overflow-hidden">
+                          <Link href={`/services/${service._id}`}>
+                            <img
+                              src={service?.image}
+                              alt="service img"
+                              // height={500}
+                              // width={500}
+                              className="w-full md:h-52 hover:opacity-70 transition hover:scale-125 duration-1000"
+                            />
+                          </Link>
+                        </div>
+
+                        <div
+                          className="mx-2 p-6 -mt-6  shadow-xl h-90"
+                          style={{ background: "white", position: "sticky" }}
+                        >
+                          <Link href={`/services/${service._id}`}>
+                            <h2 className="text-xl uppercase text-sky-500">
+                              {service.title}
+                            </h2>
+                          </Link>
+                          <hr />
+                          <p className="my-4 text-base">
+                            {service.description}
+                            <br />
+                            <Link href={`/services/${service._id}`}>
+                              <span className="underline">See details</span>
+                            </Link>
+                          </p>
+                          <h3 className="text-xl mb-2 font-bold">
+                            $ {service.price}
+                          </h3>
+                          <p className="mb-4">
+                            {" "}
+                            <PhoneOutlined /> {service.contact}
+                          </p>
+                          <hr />
+
+                          <button
+                            className="w-full bg-sky-200 font-bold hover:bg-sky-400 transition duration-700 uppercase py-2 rounded-md"
+                            onClick={() => handleAddToBookingList(service)}
+                            // disabled={buttonClicked}
+                          >
+                            book now
+                          </button>
+                        </div>
+                      </div>
+                    </Col>
+                  ))}
+                </Row>
+              )}
+            </>
+          )}
+        </Col>
+      </Row>
+
+      {/* <form className="join mx-auto w-2/3">
         <input
           type="text"
           onChange={(e) => setSearchText(e.target.value)}
           className="input input-bordered join-item w-full md:w-2/3 md:mx-auto"
           placeholder="Search here"
         />
-      </form>
+      </form> */}
 
       {/*  */}
-      <button
+      {/* <button
         onClick={() => {
           setSelectCategory("");
         }}
         className="btn w-full rounded-none text-blue-950 hover:text-white  bg-slate-400 hover:bg-blue-950 transition duration-1000"
       >
         Reset category
-      </button>
-      {categories?.map((category, i) => (
+      </button> */}
+      {/* {categories?.map((category, i) => (
         <li key={i}>
           <input
             onChange={() => setSelectCategory(category)}
@@ -60,37 +213,9 @@ const ServicePage = ({ services, categories }: IProps) => {
             {category}
           </label>
         </li>
-      ))}
+      ))} */}
 
       {/*  */}
-      {data?.length === 0 ? (
-        <h2>Data not found</h2>
-      ) : (
-        <>
-          {isLoading ? (
-            <h2>loading</h2>
-          ) : (
-            <>
-              {data?.map((service: IServices, i: number) => (
-                <>
-                  <div key={i}>
-                    <h2>{service.title}</h2>
-                    <Image
-                      src={service.image}
-                      alt="services image"
-                      width={300}
-                      height={300}
-                    />
-
-                    <p>{service.description}</p>
-                  </div>
-                  <Link href={`/services/${service._id}`}>See details</Link>
-                </>
-              ))}
-            </>
-          )}
-        </>
-      )}
     </div>
   );
 };
